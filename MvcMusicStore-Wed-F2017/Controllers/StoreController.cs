996cc7f40f6e20dc2a5c22999356a74461b55057
ifotn <rich@infrontofthenet.com>
2017-09-14 10:05:12 -0400

    lesson 2

---
+++ MvcMusicStore-Wed-F2017/Controllers/StoreController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore_Wed_F2017.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Please select a Genre";
+            return View();
+        }
+
+        // GET: Store/Browse
+        public ActionResult Browse(string genre) {
+
+            // Send genre back to the View
+            ViewBag.Genre = genre;
+            return View();
+        }
+    }
+}
